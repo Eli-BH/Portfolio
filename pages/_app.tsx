fc8ac91cf12c300c8dc9b5a285ffe86039dc3c08
Eli-b-good <eli.i.henderson95@gmail.com>
2024-05-10 13:35:07 -0400

    chore: Add sharp npm dependency for image processing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,6 @@ import { useState } from "react";
 
 export default function App({ Component, pageProps, router }: AppProps) {
   const [isOpen, setIsOpen] = useState(false);
-  console.log(isOpen);
   return (
     <div className="lg:py-24 py-8 px-12  lg:px-48 font-latoBold">
       <Navbar setIsOpen={setIsOpen} isOpen={isOpen} />
